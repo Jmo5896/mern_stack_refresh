23d7db8b62661d13439282d1a920ac61cf8fda18
Jmo5896 <jmo5896@moore-clan.net>
2019-04-30 14:01:40 -0600

    the get route is working locally for displaying whats in the data base

--- mern-todo-app/src/components/todos-list.component.js
+++ mern-todo-app/src/components/todos-list.component.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={'/edit/'+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
 
 export default class TodosList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {todos: []};
+    }
+
+    componentDidMount() {
+        //another hard coded local link
+        axios.get('http://localhost:8080/todos/')
+            .then(res => {
+                this.setState({todos: res.data});
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }
+
+    TodosList() {
+        return this.state.todos.map(function(currentTodo, i) {
+            return <Todo todo={currentTodo} key={i} />;
+        });
+    }
+
     render() {
         return (
           <div>
-              <p>Welcome to todos List Component!</p>
+              <h3>Todos List</h3>
+              <table className=" toble table-striped" style = {{ marginTop: 20 }}>
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Responsible</th>
+                        <th>Priority</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.TodosList() }
+                </tbody>
+              </table>
           </div>  
         );
     }
